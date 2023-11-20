2f91215f47ab23896994766151e9c93db3cdf135
bhavenvi <gorebhaven7@gmail.com>
2023-11-20 15:14:04 -0800

    Updated Code

--- Mydb.py
+++ Mydb.py
@@ -1,12 +1,12 @@
-import csv
-import os
-import json
 import re
+import os
+import csv
+import json
 
 def process_command(command):
     cmd_parts = command.split(maxsplit=2)
     print(cmd_parts)
-    cmd_type = cmd_parts[0]
+    cmd_type = cmd_parts[0]          
 
     if cmd_type == "create_table":
         return create_table_command(cmd_parts)
@@ -19,10 +19,11 @@ def process_command(command):
         return "Unknown command"
 
 def create_table_command(cmd_parts):
+    #Eg: create_table student id,name,age
     if len(cmd_parts) < 3:
         return "Invalid create_table command format"
     filename, columns = cmd_parts[1], cmd_parts[2]
-    columns = columns.split()
+    columns = columns.split(",")
     if not filename.endswith('.csv'):
         filename += '.csv'
 
@@ -36,11 +37,16 @@ def create_table_command(cmd_parts):
     return f"Table {filename} created with columns {', '.join(columns)}."
 
 def insert_into_command(cmd_parts):
+    #Eg: insert_into student id=1,name=Bhaven,age=25
     if len(cmd_parts) != 3:
         return "Invalid insert_into command format"
     filename, column_data = cmd_parts[1], cmd_parts[2]
-    if not os.path.exists(filename):
-        return f"Table {filename} does not exist."
+    if not filename.endswith('.csv'):
+        filename += '.csv'
+
+    # Check if the file already exists
+    if os.path.exists(filename):
+        return f"Table {filename} already exists."
 
     # Parsing column data
     data = {}
@@ -60,29 +66,47 @@ def insert_into_command(cmd_parts):
     with open(filename, 'a', newline='') as file:
         writer = csv.DictWriter(file, fieldnames=header)
         writer.writerow(ordered_data)
-        update_meta_file(filename, increment=True)
+        # update_meta_file(filename, increment=True)
 
     return f"Values inserted into {filename}."
 
 def select_command(cmd_parts):
-    if len(cmd_parts) < 3:
+    #Eg: select id,name from student where id==2
+    print(cmd_parts)
+    if len(cmd_parts) < 2:
         return "Invalid select command format"
     
     command_str = ' '.join(cmd_parts[1:])
     try:
         columns_part, rest = command_str.split(' from ')
         columns = [col.strip() for col in columns_part.split(',')]
-        filename, where_clause = rest.split(' where ')
-        conditions, logical_operator = parse_where_clause(where_clause)
+        if "where" in rest:
+            filename, where_clause = rest.split(' where ')
+            print("where",where_clause)
+            print("Coulumns", columns)
+            conditions,order_by = parse_conditions(where_clause)
+            chunk_size = 10
+            if not filename.endswith('.csv'):
+                filename += '.csv'
+
+            # Check if the file already exists
+            if os.path.exists(filename):
+                return f"Table {filename} already exists."
+
+            execute_query(filename, columns, conditions, order_by, chunk_size)
+        else:
+            filename = rest
+            if not filename.endswith('.csv'):
+                filename += '.csv'
+
+            # Check if the file already exists
+            if os.path.exists(filename):
+                return f"Table {filename} already exists."
+
+            execute_query(filename, columns)
+
     except ValueError:
         return "Invalid select command format. Ensure you use 'select col1, col2 from filename where condition'."
-
-    filename = filename.strip()
-    if not filename.endswith('.csv'):
-        filename += '.csv'
-
-    if not os.path.exists(filename):
-        return f"Table {filename} does not exist."
 
     meta_file = 'meta.json'
     if os.path.exists(meta_file):
@@ -93,102 +117,117 @@ def select_command(cmd_parts):
     else:
         chunk_line_count = 1  # Default value if meta file doesn't exist
     print("Chunk line count:", chunk_line_count)
-    process_file_in_chunks(filename, columns, chunk_line_count, conditions, logical_operator)
+    # process_file_in_chunks(filename, columns, chunk_line_count, conditions, logical_operator)
 
     return f"Select query executed on {filename}."
 
+def parse_conditions(where_clause):
+    print(where_clause)
+    "(department==HR AND salary>60000) AND (id==146 OR id==102) ORDER_BY salary DESC"
+    '''split ORDER_BY if exists'''
+    if 'ORDER_BY' in where_clause:
+        where_clause, order_by = where_clause.split('ORDER_BY')
+        order_by = order_by.split()
+    else:
+        order_by = None
+    tokens = re.split(r'(\(|\)|AND|OR|==|>|<|!=|<=|>=)', where_clause)
+    tokens = [token.strip() for token in tokens if token.strip()]
 
-def process_file_in_chunks(filename, columns, chunk_line_count, conditions, logical_operator):
+    tks = tokens.copy()
+    tks_stack = []
+    while tks:
+        token = tks.pop()
+        if token == ')':
+            temp_stack = []
+            while len(tks)>1 and tks[-1] != '(':
+                temp_stack.append(tks.pop())
+            tks_stack.append(temp_stack[::-1])
+            tks.pop()
+        else:
+            tks_stack.append(token)
+    print(tks_stack[::-1])
+    return None if not tks_stack else tks_stack[::-1], order_by
+
+def execute_query(filename,fields=None, conditions=None,order_by=None, chunk_line_count=10):
+
     with open(filename, 'r', newline='') as file:
         reader = csv.DictReader(file)
-        selected_columns = [col for col in columns if col in reader.fieldnames]
-        print(','.join(selected_columns))  # Print column headers
+        selected_columns = [col for col in fields if col in reader.fieldnames]
+        print("Prad",','.join(selected_columns))
 
-        chunk_count, chunk = 0, []
+        chunk_count, chunk,result = 0, [],[]
         for row in reader:
             chunk.append(row)
 
         if len(chunk) >= chunk_line_count:
-            filtered_chunk = [r for r in chunk if not conditions or check_condition(r, conditions, logical_operator)]
-            process_chunk(filtered_chunk, chunk_count, selected_columns)
+            filtered_chunk = [r for r in chunk if not conditions or evaluate_conditions(r, conditions)]
+            result.extend(process_chunk(filtered_chunk, chunk_count, selected_columns))
             chunk, chunk_count = [], chunk_count + 1
 
         if chunk:  # Process last chunk
-            and_operator = ' AND ' in conditions
-            filtered_chunk = [r for r in chunk if not conditions or check_condition(r, conditions, and_operator)]
-            process_chunk(filtered_chunk, chunk_count, selected_columns)
+            filtered_chunk = [r for r in chunk if not conditions or evaluate_conditions(r, conditions)]
+            result.extend(process_chunk(filtered_chunk, chunk_count, selected_columns))
+    print(result)
 
 def process_chunk(chunk, chunk_count, selected_columns):
     # print(f"Chunk {chunk_count}:")
+    ans = []
     for row in chunk:
-        print(','.join(row[col] for col in selected_columns))
+        ans.append(row)
+    return ans
 
-
-def check_condition(row, conditions, logical_operator):
-    results = []
-
-    for column_name, operator, value in conditions:
-        try:
-            row_value = row[column_name]
-            # Check each condition
-            result = False
-            if operator == '==':
-                result = row_value == value
-            elif operator in ['>', '<', '>=', '<=', '!=']:
-                result = eval(f'{float(row_value)} {operator} {float(value)}')
-            results.append(result)
-        except KeyError:
-            results.append(False)
-    if logical_operator == 'AND':
-        return all(results)
-    elif logical_operator == 'OR':
-        return any(results)
+def evaluate_condition(item, condition):
+    field, operator, value = condition
+    field = field.strip('()')
+    value = value.strip('()')
+    # print(field, operator, value)
+    if operator == '>':
+        return int(item[field]) > int(value)
+    elif operator == '<':
+        return int(item[field]) < int(value)
+    elif operator == '>=':
+        return int(item[field]) >= int(value)
+    elif operator == '<=':
+        return int(item[field]) <= int(value)
+    elif operator == '==':
+        return str(item[field]) == value
     else:
-        return results[0] if results else False
+        raise ValueError(f"Unsupported operator: {operator}")
 
-def update_meta_file(table_name, increment=False):
-    meta_file = 'meta.json'
-    meta_data = {}
+def evaluate_conditions(item, conditions):
+    if not conditions:
+        return True
 
-    # Read existing meta data
-    if os.path.exists(meta_file):
-        with open(meta_file, 'r') as file:
-            meta_data = json.load(file)
+    def eval_logical_ops(operand1, operator, operand2):
+        # print(operand1, operator, operand2)
+        if operator == 'AND':
+            return operand1 and operand2
+        elif operator == 'OR':
+            return operand1 or operand2
 
-    # Update or set the line count
-    if table_name in meta_data:
-        if increment:
-            meta_data[table_name] += 1
-    else:
-        meta_data[table_name] = 1 if increment else 0
+    def recursive_eval(conds):
+        # print(conds)
+        if not conds:
+            return True
+        
+        if 'AND' in conds or 'OR' in conds:
+            for idx, val in enumerate(conds):
+                if val in ['AND', 'OR']:
+                    left, right = conds[:idx], conds[idx+1:]
+                    if type(left[0]) == list:
+                        left_result = recursive_eval(left[0])
+                    else:
+                        left_result = recursive_eval(left)
+                    if type(right[0]) == list:
+                        right_result = recursive_eval(right[0])
+                    else:
+                        right_result = recursive_eval(right)
+                    return eval_logical_ops(left_result, val, right_result)
 
-    # Write back to the meta file
-    with open(meta_file, 'w') as file:
-        json.dump(meta_data, file)
+        return evaluate_condition(item, conds)
 
+    return recursive_eval(conditions)
 
-def parse_where_clause(where_clause):
-    if ' AND ' in where_clause:
-        operator = 'AND'
-        conditions = where_clause.split(' AND ')
-    elif ' OR ' in where_clause:
-        operator = 'OR'
-        conditions = where_clause.split(' OR ')
-    else:
-        operator = None
-        conditions = [where_clause]
-
-    parsed_conditions = []
-    for condition in conditions:
-        parts = condition.split()
-        if len(parts) != 3:
-            continue
-        column_name, condition_operator, value = parts
-        parsed_conditions.append((column_name, condition_operator, value))
-
-    return parsed_conditions, operator
-
-# Command-Line Interface
 while True:
     command = input("MyDB > ")
     if command == "exit":
